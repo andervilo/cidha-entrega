4455afa109708d0611179c42583ddc83cc7a1584
Anderson <andervilo@hotmail.com>
2022-09-27 14:24:04 -0300

    Initial version of cidha generated by generator-jhipster@7.8.1

---
+++ src/main/java/org/cidha/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package org.cidha.repository;
+
+import org.cidha.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/test/java/org/cidha/IntegrationTest.java
@@ -0,0 +1,17 @@
+package org.cidha;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.cidha.CidhaApp;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = CidhaApp.class)
+public @interface IntegrationTest {
+}
